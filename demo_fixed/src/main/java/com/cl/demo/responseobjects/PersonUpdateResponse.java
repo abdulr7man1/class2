3c7c07dd1b323a8f52c3c98bef3dc10214697a9b
abdulrhman <salimabdulrhman.739@gmail.com>
2026-08-25 12:39:07 +0400

    PersonUpdateRespons

--- demo_fixed/src/main/java/com/cl/demo/responseobjects/PersonUpdateResponse.java
+++ demo_fixed/src/main/java/com/cl/demo/responseobjects/PersonUpdateResponse.java
@@ -26,7 +26,7 @@ public class PersonUpdateResponse {
     }
 
     public static List<PersonUpdateResponse> convert(List<Person> personList) {
-
+        List<PersonUpdateResponse> responseList = new ArrayList<>();
 
 
 
