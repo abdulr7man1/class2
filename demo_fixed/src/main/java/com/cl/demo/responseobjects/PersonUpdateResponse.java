957a3bab1eff15197a9599066d637da89a302d9a
abdulrhman <salimabdulrhman.739@gmail.com>
2026-08-25 12:38:24 +0400

    PersonUpdateRespons

--- demo_fixed/src/main/java/com/cl/demo/responseobjects/PersonUpdateResponse.java
+++ demo_fixed/src/main/java/com/cl/demo/responseobjects/PersonUpdateResponse.java
@@ -19,7 +19,7 @@ public class PersonUpdateResponse {
 
     public static PersonUpdateResponse convert(Person person) {
         PersonUpdateResponse response = new PersonUpdateResponse();
-
+        response.setPersonId(person.getId().toString());
 
 
 
