03efd1c0502ea8667539275140713a4d6c9cf651
abdulrhman <salimabdulrhman.739@gmail.com>
2026-08-25 11:54:18 +0400

    PersonController

--- demo_fixed/src/main/java/com/cl/demo/controllers/PersonController.java
+++ demo_fixed/src/main/java/com/cl/demo/controllers/PersonController.java
@@ -5,7 +5,7 @@ import com.cl.demo.entities.Person;
 import com.cl.demo.requestobjects.PersonCreateRequest;
 import com.cl.demo.requestobjects.PersonUpdateRequest;
 import com.cl.demo.responseobjects.PersonCreateResponse;
-
+import com.cl.demo.responseobjects.PersonUpdateResponse;
 
 
 
