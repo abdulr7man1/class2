2b0e6f3b75898d0a14c6b238737c01aac7333a6c
abdulrhman <salimabdulrhman.739@gmail.com>
2026-08-25 12:37:05 +0400

    PersonUpdateRespons

--- demo_fixed/src/main/java/com/cl/demo/responseobjects/PersonUpdateResponse.java
+++ demo_fixed/src/main/java/com/cl/demo/responseobjects/PersonUpdateResponse.java
@@ -3,7 +3,7 @@ package com.cl.demo.responseobjects;
 import com.cl.demo.entities.Person;
 import com.cl.demo.requestobjects.PersonUpdateRequest;
 import lombok.Getter;
-
+import lombok.Setter;
 
 
 
