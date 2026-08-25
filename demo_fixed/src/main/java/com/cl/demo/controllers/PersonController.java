952aae0499461db2a09544eea64993a0484e08da
abdulrhman <salimabdulrhman.739@gmail.com>
2026-08-25 11:54:51 +0400

    PersonController

--- demo_fixed/src/main/java/com/cl/demo/controllers/PersonController.java
+++ demo_fixed/src/main/java/com/cl/demo/controllers/PersonController.java
@@ -10,7 +10,7 @@ import com.cl.demo.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-
+import java.util.List;
 
 
 
